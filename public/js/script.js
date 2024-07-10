c4f7bc4c88153cab06fbcbc98cd9cb56c9584f30
rajsahu08 <rajsahu0702@gmail.com>
2024-07-10 18:55:46 +0530

    Added a Responsive NavBar

--- public/js/script.js
+++ public/js/script.js
@@ -4,12 +4,13 @@ setInterval(() => {
   changeBackground(currHours);
 }, 60000);
 function changeBackground(currHours) {
+  console.log(currHours);
   let body = document.querySelector("body");
   if (currHours >= 5 && currHours <= 10) {
     body.style.backgroundImage = "url(/morning.jpg)";
-  } else if (currHours >= 11 && currHours <= 3) {
+  } else if (currHours >= 11 && currHours <= 15) {
     body.style.backgroundImage = "url(/afternoon.jpeg)";
-  } else if (currHours >= 4 && currHours <= 6) {
+  } else if (currHours >= 16 && currHours <= 18) {
     body.style.backgroundImage = "url(/evening.webp)";
   } else {
     body.style.backgroundImage = "url(/night.jpg)";
@@ -17,3 +18,22 @@ function changeBackground(currHours) {
   body.style.backgroundSize = "cover";
 }
 changeBackground(new Date().getHours());
+
+let modeColor = "white";
+const mode = document.querySelector(".mode");
+const innermode = document.querySelector(".innermode");
+mode.addEventListener("click", () => {
+  if (modeColor === "white") {
+    modeColor = "black";
+    mode.style.backgroundColor = "white";
+    innermode.style.backgroundColor = "black";
+    innermode.classList.remove("left");
+    innermode.classList.add("right");
+  } else {
+    modeColor = "white";
+    mode.style.backgroundColor = "black";
+    innermode.style.backgroundColor = "white";
+    innermode.classList.add("left");
+    innermode.classList.remove("right");
+  }
+});
